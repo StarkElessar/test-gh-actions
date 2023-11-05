f45b3d1dbc28ad6c811f64e53c5573d839e17df7
stark <serj.elessar@gmail.com>
2023-11-06 01:08:07 +0300

    fix: update server.js

--- server.js
+++ server.js
@@ -6,10 +6,6 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', (req, res) => {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
-
 app.listen(9000, async () => {
 	const listener = await ngrok.connect({
 		port: 9000,
